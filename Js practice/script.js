97351baeda0c9620ef38238daa2cce97f4f40cbf
LuqmanDev90 <luqmandev90@gmail.com>
2026-07-11 07:40:27 +0500

    update Js practice/script.js

--- Js practice/script.js
+++ Js practice/script.js
@@ -1,6 +1,9 @@
-let marks=[85,97,44,37,76,60];
-let i=0
-while (i<marks.length) {
-  console.log(marks[i]);
+let marks = [85, 97, 44, 37, 76, 60];
+let i = 0;
+let sum=0;
+while (i < marks.length) {
+  sum+=marks[i];
   i++;
 }
+let avg=sum/marks.length;
+console.log(avg)
